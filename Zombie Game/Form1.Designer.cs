b18a4d83983f3d40ac06d9000e23f3519a112436
howard2022 <howard.hyca@gmail.com>
2023-04-04 14:17:30 -0600

    Add project files.

---
+++ Zombie Game/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Zombie_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Roadback = new System.Windows.Forms.Panel();
+            this.zombie1 = new System.Windows.Forms.PictureBox();
+            this.explosion = new System.Windows.Forms.PictureBox();
+            this.win = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.zombie2 = new System.Windows.Forms.PictureBox();
+            this.Road1 = new System.Windows.Forms.PictureBox();
+            this.RoadAtTop = new System.Windows.Forms.PictureBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ScorePanel = new System.Windows.Forms.Label();
+            this.Intro = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.zombie3 = new System.Windows.Forms.PictureBox();
+            this.Roadback.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.win)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadAtTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Roadback
+            // 
+            this.Roadback.BackColor = System.Drawing.Color.Black;
+            this.Roadback.Controls.Add(this.zombie3);
+            this.Roadback.Controls.Add(this.zombie1);
+            this.Roadback.Controls.Add(this.explosion);
+            this.Roadback.Controls.Add(this.win);
+            this.Roadback.Controls.Add(this.player);
+            this.Roadback.Controls.Add(this.zombie2);
+            this.Roadback.Controls.Add(this.Road1);
+            this.Roadback.Controls.Add(this.RoadAtTop);
+            resources.ApplyResources(this.Roadback, "Roadback");
+            this.Roadback.Name = "Roadback";
+            this.Roadback.Paint += new System.Windows.Forms.PaintEventHandler(this.Roadback_Paint);
+            // 
+            // zombie1
+            // 
+            resources.ApplyResources(this.zombie1, "zombie1");
+            this.zombie1.Name = "zombie1";
+            this.zombie1.TabStop = false;
+            this.zombie1.Tag = "leftZombie";
+            // 
+            // explosion
+            // 
+            this.explosion.Image = global::Zombie_Game.Properties.Resources.explosion;
+            resources.ApplyResources(this.explosion, "explosion");
+            this.explosion.Name = "explosion";
+            this.explosion.TabStop = false;
+            // 
+            // win
+            // 
+            this.win.Image = global::Zombie_Game.Properties.Resources.bronze;
+            resources.ApplyResources(this.win, "win");
+            this.win.Name = "win";
+            this.win.TabStop = false;
+            this.win.Click += new System.EventHandler(this.win_Click);
+            // 
+            // player
+            // 
+            resources.ApplyResources(this.player, "player");
+            this.player.Name = "player";
+            this.player.TabStop = false;
+            this.player.Tag = "human";
+            // 
+            // zombie2
+            // 
+            this.zombie2.Image = global::Zombie_Game.Properties.Resources.zombie2;
+            resources.ApplyResources(this.zombie2, "zombie2");
+            this.zombie2.Name = "zombie2";
+            this.zombie2.TabStop = false;
+            this.zombie2.Tag = "rightZombie";
+            this.zombie2.Click += new System.EventHandler(this.zombie2_Click);
+            // 
+            // Road1
+            // 
+            this.Road1.Image = global::Zombie_Game.Properties.Resources.roadTrack;
+            resources.ApplyResources(this.Road1, "Road1");
+            this.Road1.Name = "Road1";
+            this.Road1.TabStop = false;
+            this.Road1.Click += new System.EventHandler(this.Road_Click);
+            // 
+            // RoadAtTop
+            // 
+            this.RoadAtTop.Image = global::Zombie_Game.Properties.Resources.roadTrack;
+            resources.ApplyResources(this.RoadAtTop, "RoadAtTop");
+            this.RoadAtTop.Name = "RoadAtTop";
+            this.RoadAtTop.TabStop = false;
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackColor = System.Drawing.Color.LawnGreen;
+            resources.ApplyResources(this.StartButton, "StartButton");
+            this.StartButton.Name = "StartButton";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.start);
+            // 
+            // ScorePanel
+            // 
+            resources.ApplyResources(this.ScorePanel, "ScorePanel");
+            this.ScorePanel.Name = "ScorePanel";
+            // 
+            // Intro
+            // 
+            resources.ApplyResources(this.Intro, "Intro");
+            this.Intro.Name = "Intro";
+            this.Intro.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.TimerEvent);
+            // 
+            // zombie3
+            // 
+            this.zombie3.Image = global::Zombie_Game.Properties.Resources.zombie3;
+            resources.ApplyResources(this.zombie3, "zombie3");
+            this.zombie3.Name = "zombie3";
+            this.zombie3.TabStop = false;
+            this.zombie3.Tag = "midZombie";
+            this.zombie3.Click += new System.EventHandler(this.zombie3_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Intro);
+            this.Controls.Add(this.ScorePanel);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.Roadback);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.TimerEvent);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            this.Roadback.ResumeLayout(false);
+            this.Roadback.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.explosion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.win)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Road1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RoadAtTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.zombie3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Roadback;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Label ScorePanel;
+        private System.Windows.Forms.Label Intro;
+        private System.Windows.Forms.PictureBox Road1;
+        private System.Windows.Forms.PictureBox RoadAtTop;
+        private System.Windows.Forms.PictureBox explosion;
+        private System.Windows.Forms.PictureBox win;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox zombie2;
+        private System.Windows.Forms.PictureBox zombie1;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox zombie3;
+    }
+}
+
---
+++ Zombie Game/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Zombie_Game
+{
+    public partial class Form1 : Form
+    {
+
+        int roadSpeed;
+        int zombieSpeed;
+        int playerSpeed = 12;
+        int score;
+        int zombieImage;
+
+        Random random = new Random();
+        Random zombiePosition = new Random();
+
+        bool goleft, goright;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResetGame();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Road_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Base_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            ScorePanel.Text = "Score: " + score;
+            score++;
+
+            if (goleft == true && player.Left > 10)
+            {
+                player.Left -= playerSpeed;
+            }
+            if (goright == true && player.Left < 415)
+            {
+                player.Left += playerSpeed;
+            }
+
+            //Moving the road from top to down.
+            Road1.Top += roadSpeed;
+            RoadAtTop.Top += roadSpeed;
+
+            if (RoadAtTop.Top > 519)
+            {
+                RoadAtTop.Top = -519;
+            }
+
+            if (Road1.Top > 519)
+            {
+                Road1.Top = -519;
+            }
+
+            //Zombie Moving from top to down
+            zombie1.Top += zombieSpeed;
+            zombie2.Top += zombieSpeed;
+            zombie3.Top += zombieSpeed;
+
+            if (zombie1.Top > 530)
+            {
+                changeZombie(zombie1);
+            }
+
+            if (zombie2.Top > 530)
+            {
+                changeZombie(zombie2);
+            }
+
+            if (zombie3.Top > 530)
+            {
+                changeZombie(zombie3);
+            }
+
+            //when hit the zombie, it will run the gameOver function.
+            if (player.Bounds.IntersectsWith(zombie1.Bounds) || player.Bounds.IntersectsWith(zombie2.Bounds))
+            {
+                gameOver();
+            }
+
+            //reward for different score. and it will increase the speed if getting higher scores.
+            if (score > 40 && score < 500)
+            {
+                win.Image = Properties.Resources.bronze;
+            }
+
+
+            if (score > 500 && score < 2000)
+            {
+                win.Image = Properties.Resources.silver;
+                roadSpeed = 20;
+                zombieSpeed = 22;
+            }
+
+            if (score > 2000)
+            {
+                win.Image = Properties.Resources.gold;
+                zombieSpeed = 27;
+                roadSpeed = 25;
+            }
+
+        }
+
+        //random zombie display
+        private void changeZombie(PictureBox tempZombie)
+        {
+            zombieImage = random.Next(1, 3);
+
+            switch (zombieImage)
+            {
+
+                case 1:
+                    tempZombie.Image = Properties.Resources.zombie1;
+                    break;
+                case 2:
+                    tempZombie.Image = Properties.Resources.zombie2;
+                    break;
+                case 3:
+                    tempZombie.Image = Properties.Resources.zombie3;
+                    break;
+
+            }
+
+            tempZombie.Top = zombiePosition.Next(100, 400) * -1;
+
+
+            if ((string)tempZombie.Tag == "leftZombie")
+            {
+                tempZombie.Left = zombiePosition.Next(5, 150);
+            }
+            if ((string)tempZombie.Tag == "rightZombie")
+            {
+                tempZombie.Left = zombiePosition.Next(245, 422);
+            }
+            if ((string)tempZombie.Tag == "midZombie")
+            {
+                tempZombie.Left = zombiePosition.Next(151, 244);
+            }
+
+        }
+
+        private void gameOver()
+        {
+            //Stop timer
+            gameTimer.Stop();
+            //explosion image show
+            explosion.Visible = true;
+            //explosion show on inside player image
+            player.Controls.Add(explosion);
+            //explosion position inside of player image
+            explosion.Location = new Point(-5, 5);
+            //show the explosion as anime
+            explosion.BackColor = Color.Transparent;
+
+            //show the prize
+            win.Visible = true;
+            win.BringToFront();
+
+            //start button enable to click
+            StartButton.Enabled = true;
+        }
+
+        private void Roadback_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goleft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goright = false;
+            }
+        }
+
+        private void start(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void zombie2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void win_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zombie3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ResetGame()
+        {
+            StartButton.Enabled = false;
+            explosion.Visible= false;
+            win.Visible= false;
+            goleft=false;
+            goright=false;
+            score=0;
+            win.Image = Properties.Resources.bronze;
+
+            roadSpeed=12;
+            zombieSpeed=15;
+
+            //look like coming down, so * -1.
+            zombie1.Top = zombiePosition.Next(200, 500) *-1 ;
+            //Zombie1 will appear on first lane only
+            zombie1.Left = zombiePosition.Next(5, 150) ;
+
+            zombie2.Top = zombiePosition.Next(200,500) * -1;
+            //Zombie1 will appear on second lane only
+            zombie2.Left = zombiePosition.Next(245, 422);
+
+            zombie3.Top = zombiePosition.Next(200, 500) * -1;
+            //Zombie1 will appear on second lane only
+            zombie3.Left = zombiePosition.Next(151, 244);
+
+            gameTimer.Start();
+        }
+
+
+    }
+}
